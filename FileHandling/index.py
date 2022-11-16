080b5760d07238e6e6810c7e45608dcf67b2ed81
Siyamak Abbasnezhad <siyamak1981@gmail.com>
2022-11-16 15:54:56 +0330

    File handeling and star is added

---
+++ FileHandling/index.py
@@ -0,0 +1,68 @@
+# "r" - Read - Default value. Opens a file for reading, error if the file does not exist
+
+# "a" - Append - Opens a file for appending, creates the file if it does not exist
+
+# "w" - Write - Opens a file for writing, creates the file if it does not exist
+
+# "x" - Create - Creates the specified file, returns an error if the file exists
+
+# "t" - Text - Default value. Text mode
+
+# "b" - Binary - Binary mode (e.g. images)
+
+# ====================================================
+# f = open("demofile.txt", "r")
+# print(f.read())
+
+# ====================================
+
+# f = open("demofile.txt", "r")
+# print(f.read(5))
+# # =================================
+
+# f = open("demofile.txt", "r")
+# print(f.readline())
+
+# # ===============================
+
+# f = open("demofile.txt", "r")
+
+# print(f.readline())
+# print(f.readline())
+# # ===============================
+# f = open("demofile.txt", "r")
+# for x in f:
+#   print(x)
+# # ===========================
+# f = open("demofile.txt", "r")
+# print(f.readline())
+# f.close()
+
+# # =============================
+
+# # "x" - Create - will create a file, returns an error if the file exist
+
+# # "a" - Append - will create a file if the specified file does not exist
+
+# # "w" - Write - will create a file if the specified file does not exist
+# # ======================================
+
+# f = open("demofile.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+
+# #open and read the file after the appending:
+# f = open("demofile2.txt", "r")
+# print(f.read())
+# # ===============================
+# f = open("demofile3.txt", "w")
+# f.write("Woops! I have deleted the content!")
+# f.close()
+
+# #open and read the file after the appending:
+# f = open("demofile3.txt", "r")
+# print(f.read())
+
+
+
+
--- Pathlib/index.py
+++ Pathlib/index.py
@@ -1,85 +1,98 @@
 import os
 
 
-folder = "HS"
+# folder = "HS"
 
-for key, value in enumerate(os.listdir(folder)):
-    print({key: value})
+# for key, value in enumerate(os.listdir(folder)):
+#     print({key: value})
     
-    y = f"{folder}/{value}"
-    z = f"{folder}/hotel-{key}.jpeg"
+#     y = f"{folder}/{value}"
+#     z = f"{folder}/masjed-{key}.html"
 
-    os.rename(y, z)
-
-
-
-
-for i in os.listdir():
-    if "siyamak.py" in i:
-        old = "siyamak.py"
-        new = "sort.py"
-        os.rename(old, new)
-    else:
-        print(False)
+#     os.rename(y, z)
 
 
 
-# --------------------------------------------------
+
+# for i in os.listdir():
+#     if "siyamak.py" in i:
+#         old = "siyamak.py"
+#         new = "sort.py"
+#         os.rename(old, new)
+#     else:
+#         print(False)
+
+# =====================================
+# x = [1,2,6,5,8]
+# for i in x:
+#     if i == 5:
+#         break
+#     else:
+#         print(i) 
+
+# # --------------------------------------------------
 
 
 # remove a dir that has files contained in it
-import shutil
-for key , value in enumerate(os.listdir()):
-    print(key, value)
+# import shutil
+# for key , value in enumerate(os.listdir()):
+#     new = "HS"
+#     shutil.rmtree(new)
+#     break
 
-    old = ".vscode"
-    new = ".siyamak"
-    shutil.rmtree(new)
 
-# -----------------------------------------
+# # -----------------------------------------
 import pathlib
 
-print(dir(pathlib))
-print(pathlib.WindowsPath)
-print(pathlib.__name__)
-print(pathlib.__loader__)
-print(pathlib.io)
-print(pathlib.sys)
+# print(dir(pathlib))
+# print(pathlib.WindowsPath)
+# print(pathlib.__name__)
+# print(pathlib.__loader__)
+# print(pathlib.io)
+# print(pathlib.__file__)
 
-# -----------------------------------------------
-import pathlib
+# # -----------------------------------------------
+# import pathlib
 
-print(pathlib.Path.home()) #/home/siyamak
-print(pathlib.Path.cwd()) #/home/siyamak/Documents/Algoritem
+# print(pathlib.Path.home()) #/home/siyamak
+# print(pathlib.Path.cwd()) #/home/siyamak/Documents/Algoritem
 
-# -------------------------------------------------
+# # -------------------------------------------------
 
-print(os.path.join(os.getcwd()))
-print(os.path.join(os.path.join(os.getcwd(), 'in'), 'out.py')) #/home/siyamak/Documents/Algoritem/in/out.py
-print(pathlib.Path.cwd().joinpath('in').joinpath('out.py')) #/home/siyamak/Documents/Algoritem/in/out.py
+# print(os.path.join(os.getcwd()))
+# print(os.path.join(os.path.join(os.getcwd(), 'in'), 'out.py')) #/home/siyamak/Documents/Algoritem/in/out.py
+# print(pathlib.Path.cwd().joinpath('in').joinpath('out.py')) #/home/siyamak/Documents/Algoritem/in/out.py
 
-if (pathlib.Path.cwd()).is_dir():
-    print(True)
-else:
-    print(0)
+# if (pathlib.Path.cwd()).is_dir():
+#     print(True)
+# else:
+#     print(0)
 
-if os.path.join(os.getcwd()) == pathlib.Path.cwd():
-    print(True)
-else:
-    print(0)
+# if os.path.join(os.getcwd()) == pathlib.Path.cwd():
+#     print(True)
+# else:
+#     print(0)
 
-# ----------------------------------------------------
-# copule of folder is created
-count = 0
-for i in range(10):
-    os.mkdir(os.path.join(os.getcwd(), f"siyamak{count}"))
-    count += 1
+# # ----------------------------------------------------
+# # copule of folder is created
+# count = 0
+# for i in range(5):
+#     os.mkdir(os.path.join(os.getcwd(), f"siyamak{count}"))
+#     count += 1
 
-# -----------------------------------------------------
-# move to new directory
+# # -----------------------------------------------------
+# # move to new directory
 
-newpath = "/home/siyamak/Download"
-for i in os.listdir():
-    shutil.move(i, newpath)
+# newpath = "/home/siyamak/Download"
+# for i in os.listdir():
+#     shutil.move(i, newpath)
 
 
+# ============================
+
+
+# import os
+# if os.path.exists("demofile.txt"):
+#   os.remove("demofile.txt")
+# else:
+#   print("The file does not exist")
