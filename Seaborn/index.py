b2269a256383ae84b02a093d47204eeb18f4cc7e
Siyamak Abbasnezhad <siyamak1981@gmail.com>
2023-01-13 20:54:19 +0330

    seaborn titanic dataset

---
+++ Seaborn/index.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# sns.distplot([0, 1, 2, 3, 4, 5], hist=False)
+
+# plt.show()
+# # =========================================
+
+from numpy import random
+
+# x = random.normal(size=(3, 3))
+
+# print(x)
+
+# =================================================
+# from numpy import random
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+# sns.distplot(random.normal(size=1000), hist=False)
+
+# plt.show()
+# =============================================
+# import pandas as pd
+# import numpy as np
+
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
+# dataset = sns.load_dataset('titanic')
+
+# dataset.head()
+# # sns.distplot(dataset['fare'])
+# # sns.distplot(dataset['fare'], kde=False)
+# # sns.distplot(dataset['fare'], kde=False, bins=10)
+# # sns.jointplot(x='age', y='fare', data=dataset)
+# # sns.jointplot(x='age', y='fare', data=dataset, kind='hex')
+# dataset = dataset.dropna()
+# # sns.pairplot(dataset)
+# # sns.pairplot(dataset, hue='sex')
+# sns.rugplot(dataset['fare'])
+
+# plt.show()
+# ==================================================
+# https://www.datopia.ir/%DA%A9%D8%AA%D8%A7%D8%A8%D8%AE%D8%A7%D9%86%D9%87-seaborn-%D8%A8%D8%B1%D8%A7%DB%8C-%D9%85%D8%B5%D9%88%D8%B1%D8%B3%D8%A7%D8%B2%DB%8C-%D8%AF%D8%A7%D8%AF%D9%87-%D8%AF%D8%B1-%D9%BE%D8%A7%DB%8C%D8%AA%D9%88/
+# https://www.kaggle.com/code/saikiranvepamani/seaborn-exercises/notebook
+# https://harvard-iacs.github.io/2019-CS109A/labs/lab-5/student/
+# ===================================================
+# https://github.com/mwaskom/seaborn-data
