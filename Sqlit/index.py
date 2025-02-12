a514011a66802fdd09c68123a3b620de9f187c48
pydevcasts <43889064+pydevcasts@users.noreply.github.com>
2025-02-12 15:29:14 +0330

    Update index.py

--- Sqlit/index.py
+++ Sqlit/index.py
@@ -55,4 +55,50 @@ fake = User("ariyana", "mohamadi",123456789)
 # users = session.query(User).from_statement(text(r"SELECT * FROM users WHERE password=:password").params(password=123456789)).all()
 for i in users:
   print(i.name)
+# =========================================================
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
 
+
+# Create an engine instance
+engine = create_engine('sqlite:///example.db', echo=True)  # echo=True will log all the SQL queries
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'  # Name of the table
+
+    id = Column(Integer, primary_key=True)  # Primary key
+    name = Column(String)  # Name column
+    age = Column(Integer)  # Age column
+
+    def __repr__(self):
+        return f"<User(name={self.name}, age={self.age})>"
+# Create a new user
+new_user = User(name='Alice', age=30)
+session.add(new_user)
+session.commit()  # Commit the transaction
+
+# Read users
+users = session.query(User).all()  # Get all users
+print(users)
+
+# Update a user
+user_to_update = session.query(User).filter_by(name='Alice').first()
+if user_to_update:
+    user_to_update.age = 31
+    session.commit()  # Commit the transaction
+
+# Delete a user
+user_to_delete = session.query(User).filter_by(name='Alice').first()
+if user_to_delete:
+    session.delete(user_to_delete)
+    session.commit()  # Commit the transaction
+
+# Close the session
+session.close()
